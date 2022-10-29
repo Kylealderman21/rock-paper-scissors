2348b9ac05bc0eddee6966213294515b98f53723
Kyle Alderman <k.alderman922@gmail.com>
2022-10-29 09:24:56 -0700

    Create function playRound with conditionals to choose winner of each round.

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 //User enters rock, paper, or scissors//
-//Computer randomly selects rock, paper or scissors from an array//
+
 //Conditions are checked//
 //If user selects rock and computer selects paper, user loses//
 //If user selects paper and computer selects rock, user wins//
@@ -12,11 +12,42 @@
 //If user selects scissors and computer selects scissors, ite game//
 //Repeat the game until user or computer reaches 5 wins//
 
+//Computer randomly selects rock, paper or scissors from an array//
+let playerScore = 0;
+let computerScore = 0;
 
 function getComputerChoice() {
     let choices = ['Rock', 'Paper', 'Scissors'];
-    console.log(choices[Math.floor(Math.random() * choices.length)]);
+    return choices[Math.floor(Math.random() * choices.length)];
 }
 
-getComputerChoice();
+function playRound(playerSelection, computerSelection) {
+    if ((playerSelection == 'Rock' && computerSelection == 'Scissors') ||
+        (playerSelection == 'Scissors' && computerSelection == 'Paper') ||
+        (playerSelection == 'Paper' && computerSelection == 'Rock')) {
+            console.log("You Win! " + playerSelection + " beats " + computerSelection);
+            playerScore += 1;
+             }
+    else if (playerSelection == computerSelection) {
+        console.log('Tie Game. Play Again')
+    }
 
+    else {
+        console.log('You Lose! ' + computerSelection + ' beats ' + playerSelection)
+        computerScore += 1;
+    }
+    console.log('Player score is: ' + playerScore)
+    console.log('Computer score is: '+ computerScore)
+}
+
+for (let i = 0; i < 5; i++) {
+    function game() {
+            playRound(playerSelection, computerSelection);
+    }
+}
+
+const computerSelection = getComputerChoice();
+console.log('Computer Selects: '+computerSelection);
+const playerSelection = "Rock";
+console.log('User Selects: '+playerSelection);
+game();
